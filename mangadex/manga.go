1c06e19afeddec5f89ac57d8fce627fce113cd24
Amelia <77553571+Fesaa@users.noreply.github.com>
2024-08-14 23:59:50 +0200

    Mangadex: Fix padding
    Padding for floats will ignore the decimal part for length.

--- mangadex/manga.go
+++ mangadex/manga.go
@@ -426,7 +426,7 @@ func (m *manga) comicInfo(chapter ChapterSearchData) *comicinfo.ComicInfo {
 
 func (m *manga) downloadImage(page int, chapter ChapterSearchData, url string) error {
 	//m.log.Trace("downloading image", "chapter", chapter.Attributes.Chapter, "url", url)
-	filePath := path.Join(m.chapterPath(chapter), fmt.Sprintf("page %s.jpg", pad(strconv.Itoa(page), 4)))
+	filePath := path.Join(m.chapterPath(chapter), fmt.Sprintf("page %s.jpg", padInt(page, 4)))
 	if err := downloadAndWrite(url, filePath); err != nil {
 		return err
 	}
@@ -451,8 +451,8 @@ func (m *manga) volumePath(c ChapterSearchData) string {
 }
 
 func (m *manga) chapterPath(c ChapterSearchData) string {
-	if _, err := strconv.ParseFloat(c.Attributes.Chapter, 32); err == nil {
-		chDir := fmt.Sprintf("%s Ch. %s", m.Title(), pad(c.Attributes.Chapter, 4))
+	if chapter, err := strconv.ParseFloat(c.Attributes.Chapter, 32); err == nil {
+		chDir := fmt.Sprintf("%s Ch. %s", m.Title(), padFloat(chapter, 4))
 		return path.Join(m.volumePath(c), chDir)
 	} else if c.Attributes.Chapter != "" { // Don't warm for empty chapter. They're expected to fail
 		m.log.Warn("unable to parse chapter number, not padding", "chapter", c.Attributes.Chapter, "err", err)
@@ -487,6 +487,19 @@ func downloadAndWrite(url string, path string) error {
 	}
 
 	return nil
+}
+
+func padInt(i int, n int) string {
+	return pad(strconv.Itoa(i), n)
+}
+
+func padFloat(f float64, n int) string {
+	full := strconv.FormatFloat(f, 'f', -1, 64)
+	parts := strings.Split(full, ".")
+	if len(parts) < 2 { // No decimal part
+		return pad(full, n)
+	}
+	return pad(parts[0], n) + "." + parts[1]
 }
 
 func pad(str string, n int) string {
