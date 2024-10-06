3631202dc280dbde69343bf9032ba110695bd79b
Amelia <77553571+Fesaa@users.noreply.github.com>
2024-10-06 11:32:27 +0200

    Remove config copy, is made on start

--- setup_dev_space.sh
+++ setup_dev_space.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 
-mkdir temp
-mkdir temp/Anime
-mkdir temp/LightNovels
-mkdir temp/Series
-mkdir temp/Manga
-mkdir temp/Movies
-
-EXAMPLE_CONFIG_FILE="config.yaml.example"
-CONFIG_FILE="config.yaml"
-
-if [ ! -e "$CONFIG_FILE"]; then
-  echo "Copying example config"
-  cp EXAMPLE_CONFIG_FILE CONFIG_FILE
-else
-  echo "Skipping config copy"
-fi
+mkdir -p temp
+mkdir -p temp/Anime
+mkdir -p temp/LightNovels
+mkdir -p temp/Series
+mkdir -p temp/Manga
+mkdir -p temp/Movies
