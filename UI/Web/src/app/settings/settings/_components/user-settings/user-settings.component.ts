1a6fdded1fd085a01f5e898b74ad326a9d75b46e
Amelia <77553571+Fesaa@users.noreply.github.com>
2024-11-16 10:56:48 +0100

    Fix users not loading
    API: Fix SQL Query
    WEB: Display errors
    
    API: Warn if rollback fails

--- UI/Web/src/app/settings/settings/_components/user-settings/user-settings.component.ts
+++ UI/Web/src/app/settings/settings/_components/user-settings/user-settings.component.ts
@@ -3,6 +3,7 @@ import {AccountService} from "../../../../_services/account.service";
 import {hasPermission, Perm, User, UserDto} from "../../../../_models/user";
 import {UserPreviewComponent} from "../user-preview/user-preview.component";
 import {NgIcon} from "@ng-icons/core";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-user-settings',
@@ -19,15 +20,22 @@ export class UserSettingsComponent {
   users: UserDto[] = []
   authUser: User | null = null;
 
-  constructor(private accountService: AccountService,) {
+  constructor(private accountService: AccountService,
+              private toastR: ToastrService,
+              ) {
     this.accountService.currentUser$.subscribe(user => {
       if (user) {
         this.authUser = user;
       }
     })
 
-    this.accountService.all().subscribe(users => {
-      this.users = users;
+    this.accountService.all().subscribe({
+      next: users => {
+        this.users = users;
+      },
+      error: err => {
+        this.toastR.error(err.error.error, "Unable to load all users")
+      }
     })
   }
 
