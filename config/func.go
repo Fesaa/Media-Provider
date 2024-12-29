c470ffc01852036fc9ab9b50775bb452082ca7bd
Amelia <77553571+Fesaa@users.noreply.github.com>
2024-12-29 16:51:31 +0100

    Backend: Fix HasUpdatedDB resetting on config update

--- config/func.go
+++ config/func.go
@@ -8,5 +8,6 @@ func (c *Config) Update(config Config, syncID int) error {
 	config.Version = c.Version
 	config.Secret = c.Secret
 	config.SyncId = syncID
+	config.HasUpdatedDB = c.HasUpdatedDB
 	return Save(&config)
 }
