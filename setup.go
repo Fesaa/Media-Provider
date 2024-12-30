e74b69f7a76dac55b72f8edcd200b47ac1d933f8
Amelia <77553571+Fesaa@users.noreply.github.com>
2024-12-30 12:35:32 +0100

    Don't log requests to common files
    Way too spammy, not helpful

--- setup.go
+++ setup.go
@@ -18,6 +18,7 @@ import (
 	"github.com/rs/zerolog"
 	"go.uber.org/dig"
 	"os"
+	"path"
 	"path/filepath"
 	"slices"
 	"strings"
@@ -63,10 +64,14 @@ func ApplicationProvider(params appParams) *fiber.App {
 	app.Use(prometheus.Middleware)
 
 	dontLog := []string{"/api/stats", "/", "/api/metrics"}
+	dontLogExt := []string{".js", ".html", ".css", ".svg"}
 	httpLogger := params.Log.With().Str("handler", "http").Logger()
 	app.Use(fiberzerolog.New(fiberzerolog.Config{
 		Logger: &httpLogger,
 		Next: func(c *fiber.Ctx) bool {
+			if slices.Contains(dontLogExt, path.Ext(c.Path())) {
+				return true
+			}
 			return slices.Contains(dontLog, c.Path()) || params.Cfg.Logging.Level > zerolog.InfoLevel
 		},
 		Fields: []string{
