57c2ed72a9b5f6c73e1958b9078171d5ba821d8d
Amelia <77553571+Fesaa@users.noreply.github.com>
2025-01-14 18:53:26 +0100

    Fix #54 - Rework subscriptions
    - Mobile support
    - Paginator
    - Move add button to top

--- UI/Web/src/app/subscription-manager/subscription-manager.component.ts
+++ UI/Web/src/app/subscription-manager/subscription-manager.component.ts
@@ -1,16 +1,18 @@
-import {Component, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {NavService} from "../_services/nav.service";
 import {SubscriptionService} from '../_services/subscription.service';
 import {RefreshFrequency, Subscription} from "../_models/subscription";
 import {Provider} from "../_models/page";
 import {SubscriptionComponent} from "./components/subscription/subscription.component";
 import {dropAnimation} from "../_animations/drop-animation";
+import {PaginatorComponent} from "../paginator/paginator.component";
 
 @Component({
     selector: 'app-subscription-manager',
-    imports: [
-        SubscriptionComponent,
-    ],
+  imports: [
+    SubscriptionComponent,
+    PaginatorComponent,
+  ],
     templateUrl: './subscription-manager.component.html',
     styleUrl: './subscription-manager.component.css',
     animations: [dropAnimation]
@@ -19,10 +21,15 @@ export class SubscriptionManagerComponent implements OnInit {
 
   allowedProviders: Provider[] = [];
   subscriptions: Subscription[] = [];
+  newSubscription: Subscription | null = null;
   show: boolean = true;
+
+  currentPage: number = 1;
+  pageSize: number = 5;
 
   constructor(private navService: NavService,
               private subscriptionService: SubscriptionService,
+              private cdRef: ChangeDetectorRef,
               ) {
   }
 
@@ -30,13 +37,22 @@ export class SubscriptionManagerComponent implements OnInit {
     this.navService.setNavVisibility(true)
     this.subscriptionService.all().subscribe(s => {
       this.subscriptions = s ?? [];
+      this.cdRef.detectChanges();
     })
     this.subscriptionService.providers().subscribe(providers => {
       this.allowedProviders = providers;
     })
   }
 
+  onPageChange(page: number) {
+    this.currentPage = page;
+  }
+
   remove(id: number) {
+    if (id === 0) {
+      this.newSubscription = null;
+    }
+
     this.subscriptions = this.subscriptions.filter(s => s.ID !== id);
   }
 
@@ -46,8 +62,11 @@ export class SubscriptionManagerComponent implements OnInit {
     })
   }
 
-  newSubscriptions() {
-    return this.subscriptions.filter(s => s.ID === 0);
+  toDisplay() {
+    if (!this.show) {
+      return []
+    }
+    return this.subscriptions.slice((this.currentPage - 1) * this.pageSize, this.currentPage * this.pageSize)
   }
 
   toggle() {
@@ -56,7 +75,7 @@ export class SubscriptionManagerComponent implements OnInit {
 
 
   addNew() {
-    this.subscriptions.push({
+    this.newSubscription = {
       ID: 0,
       info: {
         title: "",
@@ -67,6 +86,8 @@ export class SubscriptionManagerComponent implements OnInit {
       contentId: "",
       provider: Provider.MANGADEX,
       refreshFrequency: RefreshFrequency.Week,
-    })
+    }
   }
+
+  protected readonly Math = Math;
 }
