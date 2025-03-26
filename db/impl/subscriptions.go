645a93c0ade2b90f359943bb26793d2534ae5985
Amelia <77553571+Fesaa@users.noreply.github.com>
2025-03-27 00:39:48 +0100

    Ensure info is also (soft) deleted

--- db/impl/subscriptions.go
+++ db/impl/subscriptions.go
@@ -64,5 +64,7 @@ func (s subscriptionImpl) Update(subscription models.Subscription) error {
 }
 
 func (s subscriptionImpl) Delete(i uint) error {
-	return s.db.Delete(&models.Subscription{}, i).Error
+	return s.db.Select("Info").Delete(&models.Subscription{
+		Model: gorm.Model{ID: i},
+	}).Error
 }
