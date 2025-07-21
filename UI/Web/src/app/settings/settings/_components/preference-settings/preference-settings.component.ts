79e7968f8cb4d634916206fdafd3afcb0794762c
Amelia <77553571+Fesaa@users.noreply.github.com>
2025-07-21 11:57:52 +0200

    Stage 3

--- UI/Web/src/app/settings/settings/_components/preference-settings/preference-settings.component.ts
+++ UI/Web/src/app/settings/settings/_components/preference-settings/preference-settings.component.ts
@@ -1,75 +1,109 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, DestroyRef, inject, OnInit, signal} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {PreferencesService} from '../../../../_services/preferences.service';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {CoverFallbackMethods, Preferences} from "../../../../_models/preferences";
-import {Tooltip} from "primeng/tooltip";
+import {CoverFallbackMethods, normalize, Preferences, Tag} from '../../../../_models/preferences';
 import {ToastService} from '../../../../_services/toast.service';
-import {ToggleSwitch} from "primeng/toggleswitch";
-import {InputNumber} from "primeng/inputnumber";
-import {Button} from "primeng/button";
-import {DynastyGenresComponent} from "./dynasty-genres/dynasty-genres.component";
-import {TagsBlacklistComponent} from "./tags-blacklist/tags-blacklist.component";
-import {TranslocoDirective} from "@jsverse/transloco";
-import {Select} from "primeng/select";
-import {AgeRatingMappingsComponent} from "./age-rating-mappings/age-rating-mappings.component";
-import {WhiteListTagsComponent} from "./white-list-tags/white-list-tags.component";
-import {TagMappingsComponent} from "./tag-mappings/tag-mappings.component";
+import {TranslocoDirective} from '@jsverse/transloco';
+import {AgeRatingMappingsComponent} from './age-rating-mappings/age-rating-mappings.component';
+import {TagMappingsComponent} from './tag-mappings/tag-mappings.component';
+import {debounceTime, distinctUntilChanged, map, switchMap} from 'rxjs';
+import {SettingsItemComponent} from "../../../../shared/form/settings-item/settings-item.component";
+import {TagBadgeComponent} from "../../../../shared/_component/tag-badge/tag-badge.component";
+import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
 
 @Component({
   selector: 'app-preference-settings',
   standalone: true,
   imports: [
-    FormsModule,
     ReactiveFormsModule,
-    Tooltip,
-    ToggleSwitch,
-    InputNumber,
-    Button,
-    DynastyGenresComponent,
-    TagsBlacklistComponent,
     TranslocoDirective,
-    Select,
     AgeRatingMappingsComponent,
-    WhiteListTagsComponent,
     TagMappingsComponent,
+    SettingsItemComponent,
+    TagBadgeComponent
   ],
   templateUrl: './preference-settings.component.html',
   styleUrl: './preference-settings.component.scss'
 })
 export class PreferenceSettingsComponent implements OnInit {
 
-  preferences: Preferences | undefined;
-  displayDynastyGenresDialog: boolean = false;
-  displayBlackListTagDialog: boolean = false;
-  displayWhiteListTagDialog: boolean = false;
-  displayAgeRatingMappingDialog: boolean = false;
-  displayTagMappingDialog: boolean = false;
+  private readonly destroyRef = inject(DestroyRef);
+  private readonly preferencesService = inject(PreferencesService);
+  private readonly toastService = inject(ToastService);
+  private readonly fb = inject(FormBuilder);
 
-  constructor(private preferencesService: PreferencesService,
-              private toastService: ToastService,
-  ) {
-  }
+  preferences = signal<Preferences | undefined>(undefined);
 
-  ngOnInit(): void {
-    this.preferencesService.get().subscribe(preferences => {
-      this.preferences = preferences;
-    })
-  }
+  preferencesForm!: FormGroup;
 
-  save() {
-    if (!this.preferences) {
-      return;
-    }
-
-    this.preferencesService.save(this.preferences).subscribe({
-      next: () => {
-        this.toastService.successLoco("settings.preferences.toasts.save.success");
-      },
-      error: err => {
-        this.toastService.genericError(err.error.message);
-      }
-    })
-  }
+  displayAgeRatingMappingDialog = false;
+  displayTagMappingDialog = false;
 
   protected readonly CoverFallbackMethods = CoverFallbackMethods;
+
+  ngOnInit(): void {
+    this.preferencesService.get().subscribe((preferences: Preferences) => {
+      this.preferences.set(preferences);
+
+      this.preferencesForm = this.fb.group({
+        subscriptionRefreshHour: [preferences.subscriptionRefreshHour, [Validators.required, Validators.min(0), Validators.max(23)]],
+        logEmptyDownloads: [preferences.logEmptyDownloads],
+        convertToWebp: [preferences.convertToWebp],
+        coverFallbackMethod: [preferences.coverFallbackMethod],
+        blackList: new FormControl(preferences.blackListedTags.map(t => t.name).join(',')),
+        whiteList: new FormControl(preferences.whiteListedTags.map(t => t.name).join(',')),
+        tagToGenre: new FormControl(preferences.dynastyGenreTags.map(t => t.name).join(',')),
+      });
+
+      this.preferencesForm.valueChanges
+        .pipe(
+          takeUntilDestroyed(this.destroyRef),
+          debounceTime(300),
+          distinctUntilChanged(),
+          map(() => this.packData()),
+          switchMap(data => this.preferencesService.save(data)),
+        )
+        .subscribe({
+          error: err => this.toastService.genericError(err.error.message)
+        });
+    });
+  }
+
+  packData() {
+    const preferences = this.preferences();
+    const formValue = this.preferencesForm.value;
+
+    return {
+      ...preferences,
+      ...formValue,
+      coverFallbackMethod: parseInt(formValue.coverFallbackMethod),
+      blackListedTags: (formValue.blackList as string)
+        .split(',').map((item: string) => item.trim())
+        .filter((t: string) => t.length > 0)
+        .map(this.toTag),
+      whiteListedTags: (formValue.whiteList as string)
+        .split(',').map((item: string) => item.trim())
+        .filter((t: string) => t.length > 0)
+        .map(this.toTag),
+      dynastyGenreTags: (formValue.tagToGenre as string)
+        .split(',').map((item: string) => item.trim())
+        .filter((t: string) => t.length > 0)
+        .map(this.toTag),
+    };
+  }
+
+  toTag(s: string): Tag {
+    return {
+      name: s,
+      normalizedName: normalize(s),
+    }
+  }
+
+  breakString(s: string) {
+    if (s) {
+      return s.split(',').filter(s => s.length > 0);
+    }
+
+    return [];
+  }
 }
