37ed141a5c9e96da0417ffcf11cffad0df92e697
Amelia <77553571+Fesaa@users.noreply.github.com>
2025-12-17 18:05:15 +0100

    Dynasty & Bato url envs

--- API/config/features.go
+++ API/config/features.go
@@ -47,7 +47,9 @@ var (
 	// LimeBaseUrl override the hardcoded lime url
 	LimeBaseUrl = stringFeature("LIME_BASE_URL")
 	// YtsBaseUrl override the hardcoded yts url
-	YtsBaseUrl = stringFeature("YTS_BASE_URL")
+	YtsBaseUrl     = stringFeature("YTS_BASE_URL")
+	BatoBaseUrl    = stringFeature("BATO_BASE_URL")
+	DynastyBaseUrl = stringFeature("DYNASTY_BASE_URL")
 
 	// OtelEndpoint is the endpoint to report traces to. Passed with otlptracehttp.WithEndpointURL
 	OtelEndpoint = stringFeature("OTEL_ENDPOINT")
